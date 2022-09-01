285352d45cd2adc358ab29858c3db33624bc6ffd
Manuel Espinoza <manuelespinoza238@gmail.com>
2022-09-01 20:02:32 -0300

    init project

---
+++ App.js
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import Main from './src/Components/Main';
+
+export default function App() {
+  return (
+    <View>
+      <Main />
+    </View>
+  );
+}
---
+++ src/Components/RepositoriesList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FlatList } from 'react-native'
+import repositories from '../Data/Repositories'
+import RepositoryItem from './RepositoryItem'
+
+const RepositoryList = () => {
+  return (
+    <FlatList
+    data={repositories}
+    renderItem={({ item: repo })=>(
+        <RepositoryItem {...repo} />
+    )}
+    >
+    </FlatList>
+  )
+}
+
+export default RepositoryList;
